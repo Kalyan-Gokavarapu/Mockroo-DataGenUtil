edd294fc1011e78b8e5f5d7cfb966da22a8e9855
Kalyan Gokavarapu <gokavarapu.kalyan@hotmail.com>
2020-06-06 19:43:28 +0530

    version 2

--- Mockroo-DataGenUtil/Program.cs
+++ Mockroo-DataGenUtil/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mockroo_DataGenUtil;
+using System;
+using System.Configuration;
 using System.IO;
 using System.Text;
 
@@ -8,28 +10,63 @@ namespace DataCorrectUtil
     {
         static void Main(string[] args)
         {
-            #region current setup path
-            ////Source and Dest Files
-            //string path = @"\myhospitalapp\bin\Debug\netcoreapp3.1\";
-            //string srcFileName = "MOCK_DATA.json";
-            //string destFileName = "MOCK_PATIENTS_DATA.json";
+            //sanitizeData_v1();
+            sanitizeData_v2();
+        }
 
-            //string parentDir = Directory.GetParent(
-            //Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
+        public static void sanitizeData_v2()
+        {
+            string srcFilePath = ConfigurationManager.AppSettings["SourcePath"];
+            string destFilePath = ConfigurationManager.AppSettings["DestinationPath"];
+            string[] IdStrings = ConfigurationManager.AppSettings["IdStrings"].Split(',');
+            int IdMaxCount = Int32.Parse(ConfigurationManager.AppSettings["RepeatCount"]);
 
-            //string srcFilePath = parentDir + path + srcFileName;
-            //string destFilePath = parentDir + path + destFileName;
-            ////
-            #endregion
+            int idCounter = 1;
 
-            string srcFilePath = @"..\..\..\MOCK_DATA.json";
-            string destFilePath = @"..\..\..\MOCK_PATIENTS_DATA.json";
+            File.Delete(destFilePath);
 
-            string[] IdStrings = new[] {
-                "ExamId",
-                "id"
-            };
-            int IdMaxCount = 5;
+            string[] separatorStrings = new string[IdStrings.Length * IdMaxCount];
+            int IdCount = 0;
+            foreach (string IdString in IdStrings)
+            {
+                for (int count = 1; count <= IdMaxCount; count++, IdCount++)
+                {
+                    separatorStrings[IdCount] = IdString + "\":" + count;
+                }
+            }
+
+            using (var newFile = File.AppendText(destFilePath))
+            {
+                foreach (var line in File.ReadLines(srcFilePath))
+                {
+                    StringBuilder lineBuilder = new StringBuilder();
+                    var splitresp = line.SplitAdvaced(separatorStrings, IdMaxCount);
+                    // Build custom line - Start
+                    foreach(var item in splitresp)
+                    {
+                        var strValue = string.Empty;                        
+                        if (item.delimiter!= string.Empty && item.delimiter.IndexOf(':') > 0)
+                        {
+                            strValue = item.delimiter.Split(":")[0] + ":" + idCounter.ToString();                            
+                        } 
+                        lineBuilder.Append(item.segment + strValue);
+                        idCounter++;
+                    }
+                    // Build custom line - End
+                    newFile.WriteLine(lineBuilder);
+                }
+            }
+
+            Console.WriteLine("File Generated Successfully!");
+
+        }
+
+        public static void sanitizeData_v1()
+        {
+            string srcFilePath = ConfigurationManager.AppSettings["SourcePath"];
+            string destFilePath = ConfigurationManager.AppSettings["DestinationPath"];
+            string[] IdStrings = ConfigurationManager.AppSettings["IdStrings"].Split(',');                
+            int IdMaxCount = Int32.Parse(ConfigurationManager.AppSettings["RepeatCount"]);
 
             int idCounter = 1;
             File.Delete(destFilePath);
@@ -59,6 +96,7 @@ namespace DataCorrectUtil
                     StringBuilder lineBuilder = new StringBuilder();
 
                     var segments = line.Split(separatorStrings, StringSplitOptions.None);
+                    var advsegments = line.SplitAdvaced(separatorStrings, IdMaxCount);
                     lineBuilder.Append(segments[0]);
 
                     for (int segCount = 1; segCount < segments.Length; segCount++, idCounter++)
---
+++ Mockroo-DataGenUtil/StringExtension.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mockroo_DataGenUtil
+{
+    public static class StringExtension
+    {
+        //public class StringExtensionResponse
+        //{
+        //    public List<string> delimiterHitTrace { get; set; }
+        //    public List<string> segments { get; set; }
+
+        //}
+
+        public class SplitResponse
+        {
+            public string delimiter { get; set; }
+            public string segment{ get; set; }
+
+        }
+        //public static Dictionary<string, string> SplitAdvaced(this String inputString, string[] delimiters, int IdMaxCount)
+        public static List<SplitResponse> SplitAdvaced(this String inputString, string[] delimiters, int IdMaxCount)
+        {
+            var tempString = inputString;
+            List<string> delimiterHitTrace = new List<string>();
+            List<string> segments = new List<string>();
+
+            List<SplitResponse> splitresponse= new List<SplitResponse>();
+
+            ////Dictionary<string, string> respDict = new Dictionary<string, string>();
+
+            while (tempString.Length >0)
+            {
+                bool isFound = false;
+                foreach (var delimiter in delimiters)
+                {   
+                    var pos = tempString.IndexOf(delimiter);
+                    if (pos > 0)
+                    {
+                        ////respDict.Add(delimiter, tempString.Substring(0, pos));
+                        //delimiterHitTrace.Add(delimiter);
+                        //segments.Add(tempString.Substring(0, pos));                        
+                        splitresponse.Add(new SplitResponse{
+                            delimiter = delimiter,
+                            segment= tempString.Substring(0, pos)
+                        });
+                        tempString = tempString.Substring(pos + delimiter.Length);
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (!isFound)
+                {
+                    //delimiterHitTrace.Add(string.Empty);
+                    //segments.Add(tempString);
+                    splitresponse.Add(new SplitResponse
+                    {
+                        delimiter = string.Empty,
+                        segment = tempString
+                    });
+                    tempString = string.Empty;
+                }
+            }
+
+            //var response = new StringExtensionResponse
+            //{
+            //    delimiterHitTrace = delimiterHitTrace,
+            //    segments = segments
+            //};
+            return splitresponse;
+        }
+    }
+}
